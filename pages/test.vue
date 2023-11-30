e78069fc9c9fb79681441426303400fd09994ff5
ingfosbreak <ingfosbreak@outlook.com>
2023-11-30 17:03:29 +0700

    Update: Hologram page

--- pages/test.vue
+++ pages/test.vue
@@ -1,10 +1,18 @@
 <template>
-    <div>
-    <AVLine
-      :line-width="2"
-      line-color="white"
-      src="https://cdn.discordapp.com/attachments/919267550339158056/1179702423149883442/sample.mp3?ex=657abe7f&is=6568497f&hm=49e293e7af17b1a774e78e6a2a02827257f76d762089941efe7eea7f7ff43f54&"
-    ></AVLine>
+    <div class="min-h-screen flex flex-col justify-center items-center">
+        <div class="flex flex-col mb-8">
+            <audio ref="player" class="hidden" :src="mySource" controls autoplay crossorigin="anonymous" />
+            <canvas ref="canvas" />
+        </div>
+
+        <div class="px-[200px] text-3xl">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui perspiciatis ducimus aspernatur error eveniet quae architecto! Natus cum omnis eveniet neque quas quisquam dolor provident reiciendis distinctio nisi voluptas deleniti temporibus, iusto error magnam iste eum asperiores saepe eaque officiis!
+        </div>
+
+        <button class="mt-8" @click="autoPlay">play</button>
+    </div>
+
+    <!-- https://firebasestorage.googleapis.com/v0/b/test-3dd53.appspot.com/o/sample.mp3?alt=media&token=9bbf6889-c68e-4a8c-999f-2b59f8dd2f1b -->
     <!-- https://cdn.discordapp.com/attachments/919267550339158056/1179702423149883442/sample.mp3?ex=657abe7f&is=6568497f&hm=49e293e7af17b1a774e78e6a2a02827257f76d762089941efe7eea7f7ff43f54& -->
     <!-- <AVBars
     caps-color="#FFF"
@@ -14,16 +22,35 @@
     src="https://cdn.freesound.org/previews/712/712912_5828667-lq.mp3">
 
     </AVBars> -->
-    <AVWaveform src="https://cdn.discordapp.com/attachments/919267550339158056/1179702423149883442/sample.mp3?ex=657abe7f&is=6568497f&hm=49e293e7af17b1a774e78e6a2a02827257f76d762089941efe7eea7f7ff43f54&" />
+    <!-- <AVWaveform src="https://cdn.discordapp.com/attachments/919267550339158056/1179702423149883442/sample.mp3?ex=657abe7f&is=6568497f&hm=49e293e7af17b1a774e78e6a2a02827257f76d762089941efe7eea7f7ff43f54&" /> -->
     <!-- <AVWaveform src="https://cdn.freesound.org/previews/712/712912_5828667-lq.mp3" /> -->
-</div>
 </template>
 
 <script setup lang="ts">
-import { AVWaveform } from 'vue-audio-visual'
+definePageMeta({layout: false})
 import { AVLine } from 'vue-audio-visual';
-import { AVBars } from 'vue-audio-visual';
+import { useAVLine } from 'vue-audio-visual';
 
+
+
+const player = ref(null)
+const canvas = ref(null)
+const mySource = ref("/sample.mp3")
+
+
+
+
+//   :audio-controls="false"
+//   :line-width="1"
+//   :line-color="['#FFF', 'rgb(0,255,127)', '#00f']"
+//   src="/sample.mp3"
+//   :fft-size="256"
+useAVLine(player, canvas, { src: mySource, canvHeight: 80, canvWidth: 500, lineColor: 'white'})
+
+
+function autoPlay() {
+    player.value.play()
+}
 
 
 </script>
